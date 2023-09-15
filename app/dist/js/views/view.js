ac10f20dfde0e3071dbc4aad4292bd3b7bf86343
Renan Cadamuro Nogueira <renan.cad.nogueira@gmail.com>
2023-09-15 11:06:28 -0300

    Função tempo de execução criada

--- app/dist/js/views/view.js
+++ app/dist/js/views/view.js
@@ -1,16 +1,26 @@
 export class View {
-    constructor(seletor) {
+    constructor(seletor, escapar) {
+        this.escapar = false;
         const elemento = document.querySelector(seletor);
         if (elemento) {
             this.elemento = elemento;
         }
         else {
-            throw Error(`Seletor ${seletor} não existe no DOM. Verifique`);
+            throw Error(`Seletor ${seletor} não existe no DOM!`);
+        }
+        if (escapar) {
+            this.escapar = escapar;
         }
     }
     update(model) {
+        const t1 = performance.now();
         let template = this.template(model);
+        if (this.escapar) {
+            template = template.replace(/<script>[\s\S]*?<\/script>/, '');
+        }
         this.elemento.innerHTML = template;
+        const t2 = performance.now();
+        console.log(`Tempo de execução do método update: ${(t2 - t1) / 1000}`);
     }
 }
 //# sourceMappingURL=view.js.map
--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -1,19 +1,29 @@
-export abstract class View<T> {
+export abstract class View<T> { //Tipo genérico T. //Uma classe abstrata não pode criar uma instância dela
 
     protected elemento: HTMLElement;
+    private escapar = false;
 
-    constructor(seletor: string) {
+    constructor(seletor: string, escapar?: boolean) { // o ? é um método para usar um método de scape; Parametro opcional
         const elemento = document.querySelector(seletor);
         if (elemento) {
-            this.elemento = elemento as HTMLElement;
+            this.elemento = elemento as HTMLInputElement;
         } else {
-            throw Error(`Seletor ${seletor} não existe no DOM. Verifique`);
+            throw Error (`Seletor ${seletor} não existe no DOM!`)
+        }
+        if (escapar) {
+            this.escapar = escapar;
         }
     }
 
     public update(model: T): void {
+        const t1 = performance.now();
         let template = this.template(model);
+        if (this.escapar) {
+            template = template.replace(/<script>[\s\S]*?<\/script>/, '')
+        }
         this.elemento.innerHTML = template;
+        const t2 = performance.now();
+        console.log(`Tempo de execução do método update: ${(t2 - t1)/1000}`)
     }
 
     protected abstract template(model: T): string;
